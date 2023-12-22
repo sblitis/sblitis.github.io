903c2fc19916c2a49320d4785e0f45669e40c945
smnbrnrd <simon.bernard@univ-rouen.fr>
2023-12-22 14:55:58 +0100

    new try

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {ProjectsComponent} from "./components/projects/projects.component";
+import {PublicationsComponent} from "./components/publications/publications.component";
+import {TeachingComponent} from "./components/teaching/teaching.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'projects', component: ProjectsComponent},
+  { path: 'publications', component: PublicationsComponent},
+  { path: 'teaching', component: TeachingComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full'}
+];
